b3b15739740f98df0b1bba40e92acaddb41d8591
Luke Gartland <lukegartland@gmail.com>
2023-09-17 20:04:32 -0500

    update header text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h2 className={styles.header}> Luke and Bre 9.23.23</h2>
+      <h3 className={styles.header}>
+        Thank you for being part of our special day ❤️
+      </h3>
+      <h3 className={styles.header}>
+        Share your photos by texting them to{' '}
+        <a href='+12516992323'>251 699 2323</a>
+      </h3>
       <Photos />
     </main>
   );
